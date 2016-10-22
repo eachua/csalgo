9f6db23af9379fa70520308958ca93726b46c9aa
Eldric Earl A. Chua <ldrc_26@yahoo.com>
2016-10-22 20:56:52 +0800

    Create Coins.java

---
+++ Coins.java
@@ -0,0 +1,30 @@
+package new1;
+
+import java.util.Scanner;
+
+public class Coins {
+
+    
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    
+           double value;
+           double v1;
+           double v2;
+           double v3;
+           double v4;
+         Scanner sc = new Scanner(System.in);
+         
+         System.out.println("Enter value: ");
+         value = sc.nextDouble();
+         v1 = value / 10;
+         v2 = (value % 10)/5;
+         v3= value %5;
+         v4 = (value %1) / 0.25;
+         
+         System.out.println("10="+(int)v1);
+         System.out.println("5="+(int)v2);
+         System.out.println("1="+(int)v3);
+         System.out.println("0.25="+(int)v4);
+         
