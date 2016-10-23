5d8f00994cc07c340b26d1b8879c82e77cf47b9e
Eldric Earl A. Chua <ldrc_26@yahoo.com>
2016-10-24 00:07:42 +0800

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -4,27 +4,30 @@ import java.util.Scanner;
 
 public class Coins {
 
-    
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-    
-           double value;
-           double v1;
-           double v2;
-           double v3;
-           double v4;
-         Scanner sc = new Scanner(System.in);
-         
+        double value;
+        int v1;
+        int v2;
+        int v3;
+        int v4;
+        
+           Scanner sc = new Scanner(System.in);
          System.out.println("Enter value: ");
          value = sc.nextDouble();
-         v1 = value / 10;
-         v2 = (value % 10)/5;
-         v3= value %5;
-         v4 = (value %1) / 0.25;
-         
-         System.out.println("10="+(int)v1);
-         System.out.println("5="+(int)v2);
-         System.out.println("1="+(int)v3);
-         System.out.println("0.25="+(int)v4);
-         
+            
+            if(value>=10)
+                v1 = (int) (value/10);
+            else
+                v1=0;
+             System.out.println("10="+(int)v1);
+             
+            if(value>=5);
+            v2= (int)(value%10)/5;
+            System.out.println("5="+(int)v2);
+                
+            if(value>=1);
+            v3=(int)(value%5);
+            System.out.println("1="+(int)v3);
+            
+            if(value>=0.25);
+            v4=(int)(value%1/0.25);
+            System.out.println("0.25="+(int)v4);
